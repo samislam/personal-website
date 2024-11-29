39114158ed7fadad1d6bb82a3217825204fa20e1
Islam Yamor <islamyamor@gmail.com>
2024-11-29 16:24:12 +0300

    pageDef now uses libIcon

--- apps/web/src/types/pagedef.ts
+++ apps/web/src/types/pagedef.ts
@@ -1,11 +1,16 @@
-import { MdiIcon } from '@/components/ui/material-icon'
+import { LibraryIcon } from '@/components/common/lib-icon'
 
 export interface PageDef {
   href: string
   title?: string
   label: string
-  /** You can find more icons at: https://pictogrammers.com/library/mdi/ */
-  icon?: MdiIcon
+  /**
+   * You can find more icons at:
+   *
+   * - https://pictogrammers.com/library/mdi/
+   * - https://lucide.dev/icons/presentation
+   */
+  icon?: LibraryIcon
 }
 
 export type PagesDefs = { [k: string]: PageDef }
