592b30ed72a3aa22e19170a97f52dc977cb28185
Islam Yamor <islamyamorz@gmail.com>
2025-01-31 10:34:58 +0300

    added a simple interactive tolgee cli to web and website

---
+++ apps/website/scripts/locales.mjs
@@ -0,0 +1,50 @@
+import { concat } from 'concat-str'
+import { runCommand, Tolgee } from '@repo/scripts'
+import { select, confirm } from '@inquirer/prompts'
+
+async function main() {
+  /** @type {import('@repo/scripts').TolgeeRunMode} */
+  const mode = await select({
+    message: 'Choose an operation for the Tolgee-cli to execute:',
+    choices: [
+      {
+        name: 'Push locales',
+        value: 'push',
+        description: 'Pushes translations to Tolgee',
+      },
+      {
+        name: 'Pull locales',
+        value: 'pull',
+        description: 'Pulls translations from Tolgee',
+      },
+      {
+        name: 'Compare locales',
+        value: 'compare',
+        description: 'Compares the keys in your code project and in the Tolgee project.',
+      },
+      {
+        name: 'Sync locales',
+        value: 'sync',
+        description: concat(
+          'Synchronizes the keys in your code project and in the Tolgee project,',
+          'by creating missing keys and optionally'
+        ),
+      },
+    ],
+  })
+
+  let removeUnused = false
+  if (mode === 'sync') {
+    removeUnused = await confirm({
+      message: 'Would you like to automatically remove the unused keys from the cloud?',
+    })
+  }
+
+  runCommand(
+    new Tolgee({
+      mode,
+      removeUnused,
+    }).command
+  )
+}
+main()
--- packages/scripts/src/scripts/tolgee.ts
+++ packages/scripts/src/scripts/tolgee.ts
@@ -2,18 +2,24 @@ import { Executable } from '@/types/executable-class'
 
 export interface TolgeeOpts {
   mode: TolgeeRunMode
+  removeUnused?: boolean
 }
 export class Tolgee implements Executable {
   packageExecutable = 'tolgee'
   mode: TolgeeRunMode
+  deleteUnused?: boolean
   constructor(opts: TolgeeOpts) {
     this.mode = opts.mode
+    this.deleteUnused = opts.removeUnused
   }
   get command() {
-    return [this.packageExecutable, this.modeArg].join(' ')
+    return [this.packageExecutable, this.modeArg, this.deleteUnusedArg].join(' ')
   }
   get modeArg() {
     return this.mode
+  }
+  get deleteUnusedArg() {
+    return this.deleteUnused ? `--remove-unused` : ''
   }
 }
 
