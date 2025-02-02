566d7a86c727ed31dee8b3700ae368229124397c
Islam Yamor <islamyamorz@gmail.com>
2025-02-02 09:35:17 +0300

    added rtl support to web & fixed the case of language not syncronizing when switching between client and server components

--- apps/web/src/components/common/language-switcher.tsx
+++ apps/web/src/components/common/language-switcher.tsx
@@ -1,31 +1,64 @@
 'use client'
 
-import { useState } from 'react'
+import { Skeleton } from '../ui/skeleton'
+import { useTolgee } from '@tolgee/react'
+import { SyriaFlag } from '../icons/syria-flag'
+import { useEffect, useTransition } from 'react'
 import { AppLanguages } from '@/types/app-config'
-import { useTolgee, useTranslate } from '@tolgee/react'
-import { changeLanguage } from '@/features/change-language.action'
+import { CanadaFlag } from '../icons/canada-flag'
+import { TurkieyeFlag } from '../icons/turkieye-flag'
+import { useParams, usePathname, useRouter } from 'next/navigation'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select'
 
 export const LanguageSwitcher = () => {
-  const tolgee = useTolgee()
-  const { t } = useTranslate()
-  const [locale, setLocale] = useState(tolgee.getLanguage())
+  const router = useRouter()
+  const pathname = usePathname()
+  // const locale = useLocale()
+  const params = useParams()
+  const locale = params.locale as string
+  const { changeLanguage } = useTolgee()
+  const [isSwitching, startTransition] = useTransition()
 
-  const handleChange = async (value: AppLanguages) => {
-    await tolgee.changeLanguage(value)
-    setLocale(value)
-    changeLanguage(value)
+  const handleChange = (value: AppLanguages) => {
+    startTransition(() => {
+      // Change the server components locale
+      router.replace(pathname.replace(locale, value), { scroll: false })
+    })
   }
 
+  useEffect(() => {
+    return () => {
+      // Change the client-side components locale
+      changeLanguage(locale)
+      document.documentElement.setAttribute('dir', locale === 'ar' ? 'rtl' : 'ltr')
+    }
+  }, [changeLanguage, locale])
+
+  if (isSwitching) return <Skeleton className="h-5 w-[180px]" />
   return (
     <Select value={locale} onValueChange={handleChange}>
       <SelectTrigger className="w-[180px]">
         <SelectValue placeholder="Theme" />
       </SelectTrigger>
       <SelectContent>
-        <SelectItem value="en">{t('@t<english>')} (English)</SelectItem>
-        <SelectItem value="ar">{t('@t<arabic>')} (العربية)</SelectItem>
-        <SelectItem value="tr">{t('@t<turkish>')} (Türkçe)</SelectItem>
+        <SelectItem value="en">
+          <div className="flex items-center gap-2">
+            <CanadaFlag className="h-4 w-4" />
+            <span>English</span>
+          </div>
+        </SelectItem>
+        <SelectItem value="tr">
+          <div className="flex items-center gap-2">
+            <TurkieyeFlag className="h-4 w-4" />
+            <span>Türkçe</span>
+          </div>
+        </SelectItem>
+        <SelectItem value="ar">
+          <div className="flex items-center gap-2">
+            <SyriaFlag className="h-4 w-4" />
+            <span>العربية</span>
+          </div>
+        </SelectItem>
       </SelectContent>
     </Select>
   )
