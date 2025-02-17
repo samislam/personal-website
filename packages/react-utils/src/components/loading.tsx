a652b99e6a47ca12523714f9625fddfbf42ef04a
Islam Yamor <islamyamorz@gmail.com>
2025-02-17 17:11:09 +0300

    added loading component to react-utils

---
+++ packages/react-utils/src/components/loading.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { cn } from '../utils/cn'
+import { LibIcon, LibraryIcon } from './lib-icon'
+
+export interface LoadingProps {
+  label?: string
+  className?: string
+  iconClassName?: string
+  labelClassName?: string
+  loadingIcon?: LibraryIcon
+}
+export const Loading = (props: LoadingProps) => {
+  const { label, className, loadingIcon = 'lucide:RefreshCcwIcon' } = props
+  const { iconClassName, labelClassName } = props
+  return (
+    <div className={cn('flex flex-col items-center justify-center gap-y-2', className)}>
+      <LibIcon icon={loadingIcon} className={cn('animate-spin', iconClassName)} />
+      <span className={cn('font-bold', labelClassName)}>{label}</span>
+    </div>
+  )
+}
