3be305f80f24a3652f59e9b3bdd9bf06ec8ec708
Islam Yamor <islamyamorz@gmail.com>
2025-02-19 15:54:04 +0300

    new props to loading component

--- packages/react-utils/src/components/loading.tsx
+++ packages/react-utils/src/components/loading.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import React from 'react'
 import { cn } from '../utils/cn'
 import { LibIcon, LibraryIcon } from './lib-icon'
@@ -13,7 +14,13 @@ export const Loading = (props: LoadingProps) => {
   const { label, className, loadingIcon = 'lucide:RefreshCcwIcon' } = props
   const { iconClassName, labelClassName } = props
   return (
-    <div className={cn('flex flex-col items-center justify-center gap-y-2', className)}>
+    <div
+      className={clsx(
+        'flex flex-col items-center justify-center',
+        { 'gap-y-2': !!label },
+        className
+      )}
+    >
       <LibIcon icon={loadingIcon} className={cn('animate-spin', iconClassName)} />
       <span className={cn('font-bold', labelClassName)}>{label}</span>
     </div>
