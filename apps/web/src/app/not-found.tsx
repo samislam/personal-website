427976e74994485b0d8022c3a94c6feb56db84d3
Islam Yamor <islamyamorz@gmail.com>
2025-04-08 16:15:50 +0300

    corrected import for not-found in web app

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { buttonVariants } from '@/components/ui/button'
-import { MotionDiv, MotionSpan, MotionPath } from '@/components/common/motion'
+import { MotionDiv, MotionSpan, MotionPath } from '@repo/react-utils'
 
 export default function NotFound() {
   return (
