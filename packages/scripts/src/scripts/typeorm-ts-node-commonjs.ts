fa965f6063548a053a8ecf0e5c0886147a1c610c
Islam Yamor <islamyamorz@gmail.com>
2025-04-10 15:52:09 +0300

    fixed incorrect typeorm script missing argument in generate mode

--- packages/scripts/src/scripts/typeorm-ts-node-commonjs.ts
+++ packages/scripts/src/scripts/typeorm-ts-node-commonjs.ts
@@ -2,21 +2,32 @@ import { Executable } from '@/types/executable-class'
 
 export interface TypeORMOpts {
   dataSrcFile: string
+  migrationsDir: string
   mode: TypeORMRunMode
+  migrationName: string
 }
 export class TypeORM implements Executable {
   packageExecutable = 'typeorm-ts-node-commonjs'
   dataSrcFile: string
+  migrationsDir: string
+  migrationName: string
   mode: TypeORMRunMode
   constructor(opts: TypeORMOpts) {
     this.mode = opts.mode
     this.dataSrcFile = opts.dataSrcFile
+    this.migrationsDir = opts.migrationsDir
+    this.migrationName = opts.migrationName
   }
   get command() {
     return [this.packageExecutable, this.modeArg, this.dataSrcFileArg].join(' ')
   }
   get modeArg() {
-    return this.mode
+    switch (this.mode) {
+      case 'migration:generate':
+        return `${this.mode} ${this.migrationsDir}/${this.migrationName}`
+      default:
+        return this.mode
+    }
   }
   get dataSrcFileArg() {
     return `-d ${this.dataSrcFile}`
