360f120ed3c879f3105906888816f2d0b0545871
Islam Yamor <islamyamorz@gmail.com>
2025-08-04 11:03:14 +0300

    added images.pexels.com to known image hosts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
 const withNextIntl = createNextIntlPlugin('./src/lib/next-intl/i18n-request.ts')
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [{ hostname: 'images.pexels.com' }],
+  },
+}
 
 export default withNextIntl(nextConfig)
