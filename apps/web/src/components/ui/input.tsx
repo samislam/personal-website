fb8c46cb7b58829266f12e08306234ffe2080874
Islam Yamor <islamyamorz@gmail.com>
2025-08-10 10:05:06 +0300

    added featured input ui component

--- apps/web/src/components/ui/input.tsx
+++ apps/web/src/components/ui/input.tsx
@@ -1,22 +1,69 @@
 import * as React from 'react'
-
 import { cn } from '@repo/react-utils'
+import { type LibraryIcon, LibIcon } from '@repo/react-utils'
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
-  ({ className, type, ...props }, ref) => {
-    return (
+export type InputProps = React.ComponentProps<'input'> & {
+  icon?: LibraryIcon
+  iconClassName?: string
+  iconPosition?: 'start' | 'end'
+  startAction?: React.ReactNode
+  endAction?: React.ReactNode
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  const {
+    type,
+    icon,
+    className,
+    startAction,
+    endAction,
+    iconClassName,
+    iconPosition = 'start',
+    ...rest
+  } = props
+
+  const hasIcon = !!icon
+  const hasStartIcon = hasIcon && iconPosition === 'start'
+  const hasEndIcon = hasIcon && iconPosition === 'end'
+
+  const iconElement = icon ? (
+    <LibIcon icon={icon} className={cn('h-4 w-4 text-muted-foreground', iconClassName)} />
+  ) : null
+
+  const rootClass = cn(
+    'flex h-9 w-full items-center rounded-md border border-input bg-background py-1 text-base shadow-sm transition-colors focus-within:ring-1 focus-within:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+    // conditional inline paddings to avoid extra space before/after icons
+    hasStartIcon || startAction ? 'ps-4' : 'ps-8',
+    hasEndIcon || endAction ? 'pe-4' : 'pe-8',
+    className
+  )
+
+  return (
+    <div className={rootClass}>
+      {(hasStartIcon || startAction) && (
+        <div className="me-2 flex items-center gap-2">
+          {hasStartIcon && iconElement}
+          {startAction}
+        </div>
+      )}
+
       <input
         type={type}
-        className={cn(
-          'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-          className
-        )}
+        className="min-w-0 flex-1 border-none bg-transparent outline-none placeholder:text-muted-foreground"
         ref={ref}
-        {...props}
+        {...rest}
       />
-    )
-  }
-)
+
+      {(hasEndIcon || endAction) && (
+        <div className="ms-2 flex items-center gap-2">
+          {endAction}
+          {hasEndIcon && iconElement}
+        </div>
+      )}
+    </div>
+  )
+})
+
 Input.displayName = 'Input'
 
 export { Input }
